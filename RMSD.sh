a7a1d40ba4cee1f962374337a3b4074103aa9785
GiovanniSpagnolli <Giovanni.Spagnolli@gmail.com>
2015-10-10 12:10:43 +0200

    Calculate RMSD for all the Trial Trajectories

---
+++ RMSD.sh
@@ -0,0 +1,25 @@
+source /usr/local/gromacs/bin/GMXRC
+
+a=1
+
+while [ $a -lt 4 ] #insert_folders_number+1
+do
+	cd TRIAL_$a
+	echo 1 1 | gmx rms -s ../em-gb.gro -f md-gb.trr -o rmsd.xvg 
+	cd .. 
+	a=`expr $a + 1`
+done
+
+
+mkdir RMSD 
+
+a=1
+while [ $a -lt 4 ] #insert_folders_number+1
+do
+	cd TRIAL_$a
+	mv rmsd.xvg ../RMSD
+	cd ../RMSD
+	mv rmsd.xvg rmsd$a.xvg
+	cd .. 
+	a=`expr $a + 1`
+done
