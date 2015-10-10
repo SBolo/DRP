d4b002e80b328c9b2fd77f6d1b8dfbbc78594116
GiovanniSpagnolli <Giovanni.Spagnolli@gmail.com>
2015-10-10 12:22:32 +0200

    Update RMSD.sh

--- RMSD.sh
+++ RMSD.sh
@@ -4,7 +4,7 @@ a=1
 
 while [ $a -lt 4 ] #insert_folders_number+1
 do
-	cd TRIAL_$a
+	cd TRIAL_TRAJ_$a
 	echo 1 1 | gmx rms -s ../em-gb.gro -f md-gb.trr -o rmsd.xvg 
 	cd .. 
 	a=`expr $a + 1`
@@ -16,7 +16,7 @@ mkdir RMSD
 a=1
 while [ $a -lt 4 ] #insert_folders_number+1
 do
-	cd TRIAL_$a
+	cd TRIAL_TRAJ_$a
 	mv rmsd.xvg ../RMSD
 	cd ../RMSD
 	mv rmsd.xvg rmsd$a.xvg
