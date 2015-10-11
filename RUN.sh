89374190bf6af6ba64fd06d86c374b59ede74ec0
GiovanniSpagnolli <Giovanni.Spagnolli@gmail.com>
2015-10-11 16:10:12 +0200

    script per lanciare il tutto

---
+++ RUN.sh
@@ -0,0 +1,29 @@
+source /usr/local/gromacs/bin/GMXRC
+
+a=1
+
+while [ $a -lt 48 ] #insert_folders_number+1
+do
+	cd TRIAL_TRAJ_$a
+	echo 1 1 | gmx rms -s ../em-gb.pdb -f rmd-gb.trr -o rmsd.xvg 
+	cd .. 
+	a=`expr $a + 1`
+done
+
+
+mkdir RMSD 
+
+a=1
+while [ $a -lt 48 ] #insert_folders_number+1
+do
+	cd TRIAL_TRAJ_$a
+	mv rmsd.xvg ../RMSD
+	cd ../RMSD
+	mv rmsd.xvg rmsd$a.xvg
+	cd .. 
+	a=`expr $a + 1`
+done
+
+mkdir RMSD_selected
+
+python Selection.py
